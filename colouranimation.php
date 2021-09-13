10635d0762f2efc8fbdb1faa6c530b56b720c38c
RobertByrnes <robbyrnes@hotmail.co.uk>
2021-09-13 22:40:51 +0100

    add animation

--- colouranimation.php
+++ colouranimation.php
@@ -1,54 +1,55 @@
-<!doctype html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>jQuery UI Effects - Animate demo</title>
-  <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-  <link rel="stylesheet" href="/resources/demos/style.css">
-  <style>
-    .toggler { width: 500px; height: 200px; position: relative; }
-    #button { padding: .5em 1em; text-decoration: none; }
-    #effect { width: 240px; height: 170px; padding: 0.4em; position: relative; background: #fff; }
-    #effect h3 { margin: 0; padding: 0.4em; text-align: center; }
-  </style>
-  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-  <script>
-  $( function() {
-    var state = true;
-    $( "#button" ).on( "click", function() {
-      if ( state ) {
-        $( "#effect" ).animate({
-          backgroundColor: "#aa0000",
-          color: "#fff",
-          width: 500
-        }, 1000 );
-      } else {
-        $( "#effect" ).animate({
-          backgroundColor: "#fff",
-          color: "#000",
-          width: 240
-        }, 1000 );
-      }
-      state = !state;
-    });
-  } );
-  </script>
-</head>
-<body>
- 
-<div class="toggler">
-  <div id="effect" class="ui-widget-content ui-corner-all">
-    <h3 class="ui-widget-header ui-corner-all">Animate</h3>
-    <p>
-      Etiam libero neque, luctus a, eleifend nec, semper at, lorem. Sed pede. Nulla lorem metus, adipiscing ut, luctus sed, hendrerit vitae, mi.
-    </p>
-  </div>
-</div>
- 
-<button id="button" class="ui-state-default ui-corner-all">Toggle Effect</button>
- 
- 
-</body>
-</html>
+<?php include('pageconfig/header.php'); ?>
+
+	<header class="cd-header mt-3 mb-0 h-50">
+		<h1 class="mb-0">Colour Animation</h1>
+	</header>
+
+	<style>
+		.ui-widget-header {
+			border: none !important;
+		}
+	</style>
+
+	<div class="container mt-5">
+		<div class="row mb-3">
+			<div class="toggler col-6">
+				<div class="effect bg-light">
+					<h3 class="ui-widget-header p-3 bg-mars text-dark">Animate</h3>
+					<p class="p-3">
+						Toggle Width
+					</p>
+				</div>
+			</div>
+		</div>
+		
+		<div class="col-4">
+            <button id="toggle-button" class="btn-nbs btn-jade fade-in top w-75">Toggle Effect</button>
+        </div>
+	</div>
+
+	<script>
+		$(function() {
+			var state = true;
+			let width = 0;
+			$("#toggle-button").on("click", function() {
+				if (state) {
+					width = $('.effect').width() * 2;
+					$(".effect").animate({
+						backgroundColor: "#fff",
+						color: "#2c3531",
+						width: width
+					}, 1000);0000
+				} else {
+					width = $('.effect').width() / 2;
+					$(".effect").animate({
+						backgroundColor: "#fff",
+						color: "#2c3531",
+						width: width
+					}, 1000);
+				}
+				state = !state;
+			});
+		});
+	</script>
+
+<?php include('pageconfig/footer.php'); ?>
---
+++ tablecard.php
@@ -0,0 +1,28 @@
+<?php include('pageconfig/header.php'); ?>
+
+<table class="table table-light table-borderless table-responsive-sm">
+    <thead>
+        <tr>
+            <th scope="col"></th>
+            <th scope="col"></th>
+            <th scope="col"></th>
+            <th scope="col"></th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <th scope="row"></th>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <th scope="row"></th>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    </tbody>
+</table>
+
+<?php include('pageconfig/footer.php'); ?>
