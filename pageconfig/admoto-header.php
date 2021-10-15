e4fd8007f18bee449b61ea1dcdc93e9b34b98126
Robert Byrnes <robbyrnes@hotmail.co.uk>
2021-10-15 10:31:53 +0100

    Admoto Portal

---
+++ pageconfig/admoto-header.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="css/core/bootstrap.css">
+		<link rel="stylesheet" href="css/style-admoto.css">
+		<script src="js/core/jquery-3.6.0.min.js"></script>
+		<script src="js/core/bootstrap.bundle.js"></script>
+		
+		<title>Admoto Pricing Table</title>
+	</head>
+<body>
---
+++ pricingtable-admoto.php
@@ -0,0 +1,111 @@
+<?php include('pageconfig/admoto-header.php'); ?>
+
+<div class="container-fluid p-5">
+
+	<div class="row text-center my-5">
+		<h2>Your Plan</h2>
+	</div>
+
+	<div class="row justify-content-center">
+		<div class="col-lg-3 col-md-12 mb-4">
+			<div class="h-100 flip-card">
+				<div class="flip-card-inner">
+					<div class="flip-card-front">
+						<span class="price">12,600</span><br>/wk
+						<br>
+						<h2 class="card-title">Starter</h2>
+						<small>Individual</small>
+					</div>
+					<div class="flip-card-back">
+		
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<!-- <div class="col-lg-3 col-md-12 mb-4">
+			<div class="h-100 flip-card">
+				<div class="flip-card-inner">
+					<div class="flip-card-front">
+						<span class="price">12,600</span><br>/wk
+						<br>
+						<h2 class="card-title">Impactor</h2>
+						<small>Small Business</small>
+					</div>
+					<div class="flip-card-back">
+						<ul class="list-group list-group-flush">
+							<li class="list-group-item"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
+									<path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
+								</svg> Cras justo odio</li>
+							<li class="list-group-item"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
+									<path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
+								</svg> Dapibus ac facilisis in</li>
+							<li class="list-group-item"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
+									<path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
+								</svg> Vestibulum at eros</li>
+						</ul>
+						<button class="my-5 btn btn-outline-success btn-lg">Select</button>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+		<div class="col-lg-3 col-md-12 mb-4">
+			<div class="h-100 flip-card">
+				<div class="flip-card-inner">
+					<div class="flip-card-front">
+						<span class="price">12,600</span><br>/wk
+						<br>
+						<h2 class="card-title">Influencer</h2>
+						<small>Large Company</small>
+					</div>
+					<div class="flip-card-back">
+						<ul class="list-group list-group-flush">
+							<li class="list-group-item"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
+									<path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
+								</svg> Cras justo odio</li>
+							<li class="list-group-item"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
+									<path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
+								</svg> Dapibus ac facilisis in</li>
+							<li class="list-group-item"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
+									<path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
+								</svg> Vestibulum at eros</li>
+						</ul>
+						<button class="my-5 btn btn-outline-success btn-lg">Select</button>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+		<div class="col-lg-3 col-md-12 mb-4">
+			<div class="h-100 flip-card">
+				<div class="flip-card-inner">
+					<div class="flip-card-front">
+						<span class="price">12,600</span><br>/wk
+						<br>
+						<h2 class="card-title">Dominator</h2>
+						<small>Large Company</small>
+					</div>
+					<div class="flip-card-back">
+						<ul class="list-group list-group-flush">
+							<li class="list-group-item"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
+									<path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
+								</svg> Cras justo odio</li>
+							<li class="list-group-item"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
+									<path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
+								</svg> Dapibus ac facilisis in</li>
+							<li class="list-group-item"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
+									<path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
+								</svg> Vestibulum at eros</li>
+						</ul>
+						<button class="my-5 btn btn-outline-success btn-lg">Select</button>
+					</div>
+				</div>
+			</div>
+		</div> -->
+	<!-- </div> -->
+</div>
+
+<?php include('pageconfig/admoto-footer.php'); ?>
