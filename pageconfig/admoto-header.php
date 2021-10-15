65b5fade7c892a12da93e65ffd48ba0e3259a610
Robert Byrnes <robbyrnes@hotmail.co.uk>
2021-10-15 16:50:55 +0100

    update agent portal

--- pageconfig/admoto-header.php
+++ pageconfig/admoto-header.php
@@ -3,13 +3,13 @@
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-
+		<title>Admoto Pricing Table</title>
 		<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
 		<link rel="stylesheet" href="css/core/bootstrap.css">
 		<link rel="stylesheet" href="css/style-admoto.css">
 		<script src="js/core/jquery-3.6.0.min.js"></script>
+		<script src="js/core/jquery.flip.js"></script>
 		<script src="js/core/bootstrap.bundle.js"></script>
-		
-		<title>Admoto Pricing Table</title>
+		<script src="js/main.js"></script>		
 	</head>
 <body>
--- pricingtable-admoto.php
+++ pricingtable-admoto.php
@@ -1,111 +1,163 @@
 <?php include('pageconfig/admoto-header.php'); ?>
 
+<?php 
+	$formData = [
+		//slots
+		'displays' 			=> array(
+			'starter'			=> 3360,
+			'impactor' 			=> 10080,
+			'influencer' 		=> 16800,
+			'dominator' 		=> 33600
+		),
+		'duration' 			=> array(4,12,26,52),
+		'total_campaigns' 	=> array(1,4,12),
+		'cost_display' 		=> array(0.05),
+		'total_cost'		=> array(
+			'starter'			=> array(
+				'4'					=> 280.00,
+				'12' 				=> 798.00,
+				'26' 				=> 1638.00,
+				'52' 				=> 3094.00
+			),
+			'impactor'			=> array(
+				'4'					=> 798.00,
+				'12' 				=> 2268.00,
+				'26' 				=> 4641.00,
+				'52' 				=> 8736.00
+			),
+			'influencer'		=> array(
+				'4'					=> 1260.00,
+				'12' 				=> 3570.00,
+				'26' 				=> 7280.00,
+				'52' 				=> 13650.00
+			),
+			'dominator'			=> array(
+				'4'					=> 2380.00,
+				'12' 				=> 6720.00,
+				'26' 				=> 13650.00,
+				'52' 				=> 25480.00
+			)
+		),
+		'weekly_cost'		=> array(
+			'starter'			=> array(
+				'4'					=> 70,
+				'12' 				=> 66.5,
+				'26' 				=> 63,
+				'52' 				=> 59.5
+			),
+			'impactor'			=> array(
+				'4'					=> 66.5,
+				'12' 				=> 63,
+				'26' 				=> 59.5,
+				'52' 				=> 56
+			),
+			'influencer'		=> array(
+				'4'					=> 63,
+				'12' 				=> 59.5,
+				'26' 				=> 56,
+				'52' 				=> 52.5
+			),
+			'dominator'			=> array(
+				'4'					=> 59.5,
+				'12' 				=> 56,
+				'26' 				=> 52.5,
+				'52' 				=> 49
+			)
+		),
+		'plans'				=> array('Starter', 'Impactor', 'Influencer', 'Dominator')
+	];
+?>
+
 <div class="container-fluid p-5">
-
 	<div class="row text-center my-5">
-		<h2>Your Plan</h2>
+		<h1>Your Plan</h1>
 	</div>
-
-	<div class="row justify-content-center">
+	<div class="pricing-switcher">
+		<p class="fieldset">
+			<input type="radio" name="duration-1" data-name="Starter" value="starter" id="starter-1" checked>
+			<label id="Starter" for="starter-1">Starter<span class="cd-switch">Starter</span></label>
+			<input type="radio" name="duration-1" data-name="Impactor" value="impactor" id="impactor-1">
+			<label id="Impactor" for="impactor-1">Impactor</label>
+			<input type="radio" name="duration-1" data-name="Influencer" value="influencer" id="influencer-1">
+			<label id="Influencer" for="influencer-1">Influencer</label>
+			<input type="radio" name="duration-1" data-name="Dominator" value="dominator" id="dominator-1">
+			<label id="Dominator" for="dominator-1">Dominator</label>
+		</p>
+	</div>
+	<!-- flip-card -->
+	<div id="your-plan" class="row justify-content-center mt-5">
 		<div class="col-lg-3 col-md-12 mb-4">
 			<div class="h-100 flip-card">
-				<div class="flip-card-inner">
-					<div class="flip-card-front">
-						<span class="price">12,600</span><br>/wk
-						<br>
-						<h2 class="card-title">Starter</h2>
-						<small>Individual</small>
+				<div id="flip" class="flip-card-inner">
+					<div class="flip-side front">
+						<div class="flip-card-style rounded">
+							<form id="campaign-form" method="" action="">
+								<div class="form-group mb3">
+									<label for="displays" class="form-label"><h2 class="card-title">Displays</h2></label>
+									<span><input id="displays" type="text" class="form-control w-25 mx-auto text-center" value="" name="" readonly /><small class="ms-1">/wk</small></span>
+								</div>
+								<div class="form-group mb-3">
+									<label for="duration" class="form-label"><h2 class="card-title">Duration</h2></label>
+									<select id="duration" class="form-control w-25 mx-auto text-center" name="" >
+										<?php foreach($formData['duration'] as $duration) { echo "<option value=".$duration.">$duration<span id='week-text'> Weeks</span></option>"; } ?>
+									</select>
+								</div>
+								<div class="form-group mb-3">
+									<label for="total_campaigns" class="form-label"><h2 class="card-title">Total Campaigns</h2></label>
+									<input id="total_campaigns" type="text" class="form-control w-25 mx-auto text-center" value="12" name="" readonly />
+								</div>
+								<div class="form-group mb-3">
+									<label for="total_displays" class="form-label"><h2 class="card-title">Total Displays</h2></label>
+									<input id="total_displays" type="text" class="form-control w-25 mx-auto text-center" value="" name="" readonly />
+								</div>
+								<div class="form-group mb-3">
+									<label for="cost_display" class="form-label"><h2 class="card-title">Cost / Display</h2></label>
+									<input id="cost_display" type="text" class="form-control w-25 mx-auto text-center" value="" name="" readonly />
+								</div>
+							</form>
+						</div>
 					</div>
-					<div class="flip-card-back">
-		
+					<div class="flip-side back">
+
 					</div>
 				</div>
 			</div>
 		</div>
-
-		<!-- <div class="col-lg-3 col-md-12 mb-4">
-			<div class="h-100 flip-card">
-				<div class="flip-card-inner">
-					<div class="flip-card-front">
-						<span class="price">12,600</span><br>/wk
-						<br>
-						<h2 class="card-title">Impactor</h2>
-						<small>Small Business</small>
-					</div>
-					<div class="flip-card-back">
-						<ul class="list-group list-group-flush">
-							<li class="list-group-item"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
-									<path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
-								</svg> Cras justo odio</li>
-							<li class="list-group-item"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
-									<path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
-								</svg> Dapibus ac facilisis in</li>
-							<li class="list-group-item"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
-									<path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
-								</svg> Vestibulum at eros</li>
-						</ul>
-						<button class="my-5 btn btn-outline-success btn-lg">Select</button>
-					</div>
-				</div>
-			</div>
-		</div>
-
-
-		<div class="col-lg-3 col-md-12 mb-4">
-			<div class="h-100 flip-card">
-				<div class="flip-card-inner">
-					<div class="flip-card-front">
-						<span class="price">12,600</span><br>/wk
-						<br>
-						<h2 class="card-title">Influencer</h2>
-						<small>Large Company</small>
-					</div>
-					<div class="flip-card-back">
-						<ul class="list-group list-group-flush">
-							<li class="list-group-item"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
-									<path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
-								</svg> Cras justo odio</li>
-							<li class="list-group-item"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
-									<path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
-								</svg> Dapibus ac facilisis in</li>
-							<li class="list-group-item"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
-									<path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
-								</svg> Vestibulum at eros</li>
-						</ul>
-						<button class="my-5 btn btn-outline-success btn-lg">Select</button>
-					</div>
-				</div>
-			</div>
-		</div>
-
-
-		<div class="col-lg-3 col-md-12 mb-4">
-			<div class="h-100 flip-card">
-				<div class="flip-card-inner">
-					<div class="flip-card-front">
-						<span class="price">12,600</span><br>/wk
-						<br>
-						<h2 class="card-title">Dominator</h2>
-						<small>Large Company</small>
-					</div>
-					<div class="flip-card-back">
-						<ul class="list-group list-group-flush">
-							<li class="list-group-item"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
-									<path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
-								</svg> Cras justo odio</li>
-							<li class="list-group-item"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
-									<path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
-								</svg> Dapibus ac facilisis in</li>
-							<li class="list-group-item"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
-									<path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
-								</svg> Vestibulum at eros</li>
-						</ul>
-						<button class="my-5 btn btn-outline-success btn-lg">Select</button>
-					</div>
-				</div>
-			</div>
-		</div> -->
-	<!-- </div> -->
+	</div>
 </div>
+
+<script>
+	$(() => {
+		$('#flip').flip({trigger:'manual'})
+		let radios = $(document).find('input[type="radio"]')
+		$(radios).on('click', (event) => {
+			$("input[name='duration-1']").each(function() {
+				$(this).attr('checked', false)
+				$(this).parent().find('span').remove('.cd-switch')
+			})
+			let html = '<span class="cd-switch">' + $(event.target).data('name') + '</span>'
+			$(html).hide().appendTo('#' + $(event.target).data('name') + '').fadeIn(150)
+			$('#flip').flip('toggle', {reverse: false})
+			// if ($('#flip-side').hasClass('front')) {
+			// 	$('.front >.child').appendTo($('.back'))
+			// } else {
+			// 	$('.front >.child').appendTo($('.back'))
+			// }
+		})
+		let duration = parseInt($('#duration').val())
+		let displays = parseInt('<?php echo $formData['displays']['starter']; ?>')
+		$('#displays').val(displays)
+		$('#total_displays').val(duration * displays)
+		$('#duration').on('change', () => {
+			duration = parseInt($('#duration').val())
+			$('#total_displays').val(duration * displays)
+		})
+		$('#cost_display').val('<?php echo $formData['cost_display'][0]; ?>')
+
+		$('input[type="radio"]').on('change', () => {
+
+		})
+	})
+</script>
 
 <?php include('pageconfig/admoto-footer.php'); ?>
